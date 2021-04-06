458acebd6819ec9af9809c328acec618efc53f7d
djennings <deanjennings@rightpoint.com>
2021-04-06 05:55:45 -0400

    initial styles for activity form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      console.log("did mount2");
+      const learnosityObject = {
+        mode: "activity_edit",
+        reference: "custom-name-reference-2",
+        config: {
+          activity_edit: {
+            back: false,
+            tags_on_create: [
+              {
+                type: "Institution",
+                name: "app",
+              },
+            ],
+            reference: {
+              edit: false,
+            },
+          },
+        },
+        user: {
+          id: "demos-site",
+          firstname: "Demos",
+          lastname: "User",
+          email: "demos@learnosity.com",
+        },
+      };
+      const learnosityResponse = await axios.post(
+        "https://apiv2.pa-assessments.aula.education/learnosity/sign",
+        {
+          serviceType: "author",
+          learnosityObject,
+        },
+        {
+          headers: {
+            "x-session-token": "r:e0116400a87ac79512555073284b5c24",
+          },
+        }
+      );
+      window.LearnosityAuthor.init(learnosityResponse.data.learnosityResponse);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="learnosity-author"></div>
     </div>
   );
 }
